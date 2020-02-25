c75c149eb78674d511581f2238fc5b95ea4fd32d
Yamna Melki <melkiyamna@protonmail.com>
2020-02-25 10:12:25 +0100

    patch all_face_encoding function bug

--- classifier.py
+++ classifier.py
@@ -17,7 +17,7 @@ user_faces_name = np.append([], dir_name)
 
 
 # Encode all users
-def face_encoding():
+def all_face_encoding():
     # Get list of users directories names
     all_user = np.append([], listdir('training-data'))
 
@@ -47,7 +47,7 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 timeout = time.time() + 20
-face_encoding()
+all_face_encoding()
 
 while True:
     # Grab a single frame of video
@@ -110,98 +110,7 @@ while True:
     cv2.imshow('Video', frame)
 
     if time.time() > timeout:
-
-        break
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
-
-
-# detect faces and recognize them
-def main_process():
-    # Initialize some variables
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    process_this_frame = True
-    timeout = time.time() + 20
-    while True:
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
-
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.5)
-                name = "Ptdr t ki"
-
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-
-                if matches[best_match_index]:
-                    name = user_faces_name[best_match_index]
-
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            font = cv2.FONT_HERSHEY_DUPLEX
-            if not name == 'Ptdr t ki':
-                if not path.exists('training-data/{0}/{1}_encoding2.txt'.format(name, name)):
-                    # mettre a jour photo si date > 1 mois
-                    location_for_update = 'training-data/{0}/{1}_encoding.txt'.format(name, name)
-                    today = datetime.datetime.today()
-                    modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(location_for_update))
-                    duration = today - modified_date
-                    if duration.days > 30:
-                        face_vectorization(frame, name)
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 0, 0), 1)
-
-        # Display the resulting image
-        cv2.imshow('Video', frame)
-
-        if time.time() > timeout:
-            break
-
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            # Release handle to the webcam
-            video_capture.release()
-            cv2.destroyAllWindows()
-            break
-
-        face_encoding()
+        all_face_encoding()
         timeout = time.time() + 60 * 60
 
     # Hit 'q' on the keyboard to quit!
@@ -211,3 +120,4 @@ def main_process():
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
+
