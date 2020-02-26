3d1a38a678293eb0286c55a455389852b21d2ac3
Yamna Melki <melkiyamna@protonmail.com>
2020-02-26 18:01:19 +0100

    patching face_update

--- classifier.py
+++ classifier.py
@@ -1,38 +1,57 @@
-import face_recognition
-import cv2
-import numpy as np
+#!/usr/bin/python
+import datetime
 import os
+from os import path, listdir
+import face_recognition
+import numpy as np
+import cv2
+import time
+import sys
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
+known_face_encodings = []
 
 # Get list of users directories names
 dir_path = 'training-data'
-
-dir_name = os.listdir(dir_path)
+dir_name = listdir(dir_path)
 user_faces_name = np.append([], dir_name)
 
-known_face_encodings = []
 
-# Encode all users images
+# Encode all users
+def all_face_encoding():
+    # Get list of users directories names
+    all_user = np.append([], listdir('training-data'))
+    for user in all_user:
+        #  if path.exists("training-data/{0}/{1}_encoding2.txt".format(name, name)):
+        # effacer image encoding et rename face encoding2 en encoding
+        if path.exists("training-data/{0}/{1}.jpg".format(user, user)):
+            user_image = face_recognition.load_image_file("training-data/{0}/{1}.jpg".format(user, user))
+            user_face_encoding = face_recognition.face_encodings(user_image)[0]
+            os.remove('training-data/{0}/{1}_encoding.txt'.format(user, user))
+            np.savetxt('training-data/{0}/{1}_encoding.txt'.format(user, user), user_face_encoding)
+            os.remove("training-data/{0}/{1}.jpg".format(user, user))
+        else:
+            user_face_encoding = np.loadtxt('training-data/{0}/{1}_encoding.txt'.format(user, user))
+            known_face_encodings.append(user_face_encoding)
 
-for name in user_faces_name:
-    user_image_path = "training-data/{0}/{1}.jpg".format(name, name)
-    user_encoding_path = "training-data/{0}/{1}_encoding.txt".format(name, name)
-    if not os.path.exists(user_encoding_path):
-        user_image = face_recognition.load_image_file(user_image_path)
-        user_face_encoding = face_recognition.face_encodings(user_image)[0]
-        np.savetxt(user_encoding_path, user_face_encoding)
-        os.remove(user_image_path)
-    else:
-        user_face_encoding = np.loadtxt(user_encoding_path)
-        known_face_encodings.append(user_face_encoding)
+    del all_user
+
+
+# update known faces
+def face_update(frame, name):
+    img_path = 'training-data/{0}/{1}.jpg'
+    cv2.imwrite(img_path.format(name, name), frame)
+    all_face_encoding()
+    os.execl(sys.executable, sys.executable, * sys.argv)
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
+timeout = time.time() + 60 * 60 * 24
+all_face_encoding()
 
 while True:
     # Grab a single frame of video
@@ -56,6 +75,11 @@ while True:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.5)
             name = "Ptdr t ki"
 
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
@@ -75,23 +99,30 @@ while True:
         bottom *= 4
         left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        today = datetime.datetime.today()
         font = cv2.FONT_HERSHEY_DUPLEX
+        if not name == 'Ptdr t ki' and not os.path.isfile("training-data/{0}/{1}.jpg".format(name, name)):
+            location_for_update = 'training-data/{0}/{1}_encoding.txt'.format(name, name)
+            modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(location_for_update))
+            duration = today - modified_date
+            if duration.seconds > 60:
+                # mettre a jour photo si date > 1 mois
+                face_update(frame, name)
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-        if name == 'Ptdr t ki':
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 0, 255), 1)  # Ne pas ouvrir
-        else:
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 255, 0), 1)  # Ouvrir
-            # command = os.popen('open the porte please')
-            # print(command.read())
-            # print(command.close())
 
-    # Display the resulting image
+        datestamp = today.strftime("%m/%d/%Y, %H:%M:%S")
+        date = today.strftime("%m-%d-%Y")
+        mode = 'a' if os.path.isfile("log/" + date) else 'w'
+        with open("log/" + date, mode) as log:
+            log.write(name + " / face / " + datestamp + "\n")
+            log.close()
+
+        # Display the resulting image
     cv2.imshow('Video', frame)
+
+    if time.time() > timeout:
+        all_face_encoding()
+        timeout = time.time() + 60 * 60 * 24
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
