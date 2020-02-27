d501adc3466eeab60ce45a4d79fff05a99334818
novaedra <leofouquier20111999@gmail.com>
2020-02-27 15:05:19 +0100

    replace opencv with pygame

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import test
-import test.camera
-from test.locals import *
+import pygame
+import pygame.camera
+from pygame.locals import *
 
 DEVICE = '/dev/video0'
 SIZE = (640, 480)
@@ -8,24 +8,24 @@ FILENAME = 'capture.png'
 
 
 def camstream():
-    test.init()
-    test.camera.init()
-    display = test.display.set_mode(SIZE, 0)
-    camera = test.camera.Camera(DEVICE, SIZE)
+    pygame.init()
+    pygame.camera.init()
+    display = pygame.display.set_mode(SIZE, 0)
+    camera = pygame.camera.Camera(DEVICE, SIZE)
     camera.start()
-    screen = test.surface.Surface(SIZE, 0, display)
+    screen = pygame.surface.Surface(SIZE, 0, display)
     capture = True
     while capture:
         screen = camera.get_image(screen)
         display.blit(screen, (0, 0))
-        test.display.flip()
-        for event in test.event.get():
+        pygame.display.flip()
+        for event in pygame.event.get():
             if event.type == QUIT:
                 capture = False
             elif event.type == KEYDOWN and event.key == K_s:
-                test.image.save(screen, FILENAME)
+                pygame.image.save(screen, FILENAME)
     camera.stop()
-    test.quit()
+    pygame.quit()
     return
 
 
