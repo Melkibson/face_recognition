992308acc118124b905f9dfea0bad705d6f70749
novaedra <leofouquier20111999@gmail.com>
2020-02-27 11:08:43 +0100

    add delay to log

--- classifier.py
+++ classifier.py
@@ -112,17 +112,17 @@ while True:
         if not os.path.exists('log'):
             os.makedirs('log')
 
-        print(face_log)
+        print(time.time())
         if name not in face_log:
-            print("ça passe")
             face_log[name] = time.time() + 60
+            print("mon temps : " + str(face_log[name]))
         if time.time() > face_log[name]:
             mode = 'a' if os.path.isfile("log/" + date) else 'w'
             with open("log/" + date, mode) as log:
                 log.write(name + " / face / " + datestamp + "\n")
                 log.close()
-            face_log[name] = today.strftime("%H:%M:%S")
-            print(name + " - " + face_log[name])
+            face_log[name] = time.time() + 60
+            print("log créer")
 
     # Display the resulting image
     cv2.imshow('Video', frame)
