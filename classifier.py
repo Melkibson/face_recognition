c8d54451fd32a3177b1fc95e9e7ef7fe88c24c8e
novaedra <leofouquier20111999@gmail.com>
2020-02-28 15:58:09 +0100

    debug pygame camera

--- classifier.py
+++ classifier.py
@@ -8,7 +8,6 @@ import picamera
 import time
 
 # Get a reference to webcam #0 (the default one)
-
 camera = picamera.PiCamera()
 camera.resolution = (720, 480)
 frame = np.empty((240, 320, 3), dtype=np.uint8)
@@ -55,17 +54,11 @@ while True:
     # Grab a single frame of video
     camera.capture(frame, format="rgb")
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
-
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_locations = face_recognition.face_locations(frame)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
