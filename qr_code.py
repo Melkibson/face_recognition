f857795ef2129215ef806f840b51f02278c8c4ed
Yamna Melki <melkiyamna@protonmail.com>
2020-02-28 14:01:35 +0100

    opening door with qrcode

--- qr_code.py
+++ qr_code.py
@@ -1,33 +1,26 @@
 import pygame
-from pygame import camera
-from pygame.locals import *
+import pygame.camera
 import qrtools
 
-pygame.init()
-camera.init()
+DEVICE = '/dev/video0'
 
 user_code = 'http://google.fr'
 
 
 def qr_code_reader():
-    camlist = camera.list_cameras()
+    pygame.init()
+    pygame.camera.init()
 
-    if camlist:
-        cam = camera.Camera(camlist[0], (640, 480))
-        image = cam.get_image()
-        qr = qrtools.QR()
-        qr.decode(image)
-        s = qr.data
-        print("The decoded QR code is: %s" % s)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                print("pressed w")
-            if event.key == pygame.K_s:
-                print("pressed s")
+    cam = pygame.camera.Camera(DEVICE, (640, 480))
+    image = cam.get_image()
+    qr = qrtools.QR()
+    qr.decode(image)
+    s = qr.data
+    print("The decoded QR code is: %s" % s)
 
+    cam.stop()
+    pygame.quit()
+    return
 
 
 qr_code_reader()
