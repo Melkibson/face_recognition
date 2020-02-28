58f062b906bd8ae16141bf902c1dd3d7e45b771c
Yamna Melki <melkiyamna@protonmail.com>
2020-02-28 14:09:35 +0100

    opening door with qrcode

--- qr_code.py
+++ qr_code.py
@@ -3,6 +3,7 @@ import pygame.camera
 import qrtools
 
 DEVICE = '/dev/video0'
+SIZE = (640, 480)
 
 user_code = 'http://google.fr'
 
@@ -10,19 +11,28 @@ user_code = 'http://google.fr'
 def qr_code_reader():
     pygame.init()
     pygame.camera.init()
-    display = pygame.camera.Camera(DEVICE, (640, 480))
-    cam = pygame.camera.Camera(DEVICE, (640, 480))
-    cam.start()
-    image = pygame.surface.Surface((640, 480), display )
-    image = cam.get_image(image)
+    display = pygame.display.set_mode(SIZE, 0)
+    camera = pygame.camera.Camera(DEVICE, SIZE, "RGB")
+    camera.start()
+    screen = pygame.surface.Surface(SIZE, 0, display)
     capture = True
     while capture:
+        screen = camera.get_image(screen)
+        display.blit(screen, (0, 0))
+        pygame.display.flip()
         qr = qrtools.QR()
-        qr.decode(image)
+        qr.decode(screen)
         s = qr.data
-    print("The decoded QR code is: %s" % s)
-
-    cam.stop()
+        print("The decoded QR code is: %s" % s)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                capture = False
+                return
+            elif event.type == pygame.KEYDOWN:
+                # If pressed key is ESC quit program
+                if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                    capture = False
+    camera.stop()
     pygame.quit()
     return
 
