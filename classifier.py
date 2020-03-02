6018ef2ff5177d967e33b5808baa00d4fbdb25ff
novaedra <leofouquier20111999@gmail.com>
2020-03-02 13:49:48 +0100

    replace cv2 with picamera

--- classifier.py
+++ classifier.py
@@ -56,18 +56,14 @@ while True:
     # Grab a single frame of video
     camera.capture(rawCapture, format='rgb')
     frame = rawCapture.array
-    cv2.imshow("Image", frame)
-
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Only process every other frame of video to save time
     if process_this_frame:
         print("process new frame")
 
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(small_frame)
-        face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+        face_locations = face_recognition.face_locations(frame)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
         face_names = []
         for face_encoding in face_encodings:
             print(str(len(face_encodings)) + " face(s)")
