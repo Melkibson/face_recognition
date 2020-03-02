dd3599fb26008cfa62764140b7aa669c2076f2bc
novaedra <leofouquier20111999@gmail.com>
2020-03-02 10:08:56 +0100

    replace cv2 with picamera

--- classifier.py
+++ classifier.py
@@ -4,19 +4,12 @@ from os import path, listdir
 import face_recognition
 import numpy as np
 import cv2
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import time
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-
-# allow the camera to warmup
-time.sleep(0.1)
-
+# import pygame
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
 known_face_encodings = []
-
 # Get list of users directories names
 dir_path = 'training-data'
 dir_name = listdir(dir_path)
@@ -50,48 +43,36 @@ face_names = []
 face_log = {}
 process_this_frame = True
 reset = time.time() + 60 * 60 * 24
-
 all_face_encoding()
 
 while True:
-    # grab an image from the camera
-    camera.capture(rawCapture, format="bgr")
-    frame = rawCapture.array
-
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
-
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.5)
             name = "Ptdr t ki"
-
             # # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
             #     first_match_index = matches.index(True)
             #     name = known_face_names[first_match_index]
-
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
-
             if matches[best_match_index]:
                 name = user_faces_name[best_match_index]
-
             face_names.append(name)
-
     process_this_frame = not process_this_frame
-
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
@@ -99,7 +80,6 @@ while True:
         right *= 4
         bottom *= 4
         left *= 4
-
         today = datetime.datetime.today()
         if not name == 'Ptdr t ki' and not os.path.isfile("training-data/{0}/{1}.jpg".format(name, name)):
             location_for_update = 'training-data/{0}/{1}_encoding.txt'.format(name, name)
@@ -108,16 +88,11 @@ while True:
             if duration.seconds > 30:
                 # mettre a jour photo si date > 1 mois
                 cv2.imwrite('training-data/{0}/{1}.jpg'.format(name, name), frame)
-            os.system("python3 lock_control.py authorized " + name)
-        else:
-            os.system("python3 lock_control.py unauthorized")
         print(name)
-
         datestamp = today.strftime("%m/%d/%Y, %H:%M:%S")
         date = today.strftime("%m-%d-%Y")
         if not os.path.exists('log'):
             os.makedirs('log')
-
         if name not in face_log or time.time() > face_log[name]:
             face_log[name] = time.time() + 10
             mode = 'a' if os.path.isfile("log/" + date) else 'w'
@@ -132,16 +107,13 @@ while True:
             # pygame.mixer.music.stop()
             # pygame.quit()
     # Display the resulting image
-    cv2.imshow("Image", frame)
-
+    cv2.imshow('Video', frame)
     if time.time() > reset:
         all_face_encoding()
         reset = time.time() + 60 * 60 * 24
-
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
