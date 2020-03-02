8b3402c2a35dc4618c3969e3a7b98fc70045bea8
novaedra <leofouquier20111999@gmail.com>
2020-03-02 14:19:13 +0100

    replace cv2 with picamera

--- classifier.py
+++ classifier.py
@@ -1,20 +1,22 @@
-import datetime
 import os
-from os import path, listdir
+from os import listdir, path
 import face_recognition
 import numpy as np
 import cv2
 import time
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+import datetime
+import pygame
+import pygame.camera
+from pygame.locals import *
+
+# initialize the camera and grab a reference to the raw camera capture
+pygame.init()
+pygame.camera.init()
 
 # Get a reference to webcam #0 (the default one)
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 5
-rawCapture = PiRGBArray(camera, size=(640, 480))
-
-time.sleep(1)
+camlist = pygame.camera.list_cameras()
+cam = pygame.camera.Camera(camlist[0], (640, 480))
+cam.start()
 
 known_face_encodings = []
 # Get list of users directories names
@@ -27,7 +29,6 @@ user_faces_name = np.append([], dir_name)
 def all_face_encoding():
     # Get list of users directories names
     all_user = np.append([], listdir('training-data'))
-
     for user in all_user:
         #  if path.exists("training-data/{0}/{1}_encoding2.txt".format(name, name)):
         # effacer image encoding et rename face encoding2 en encoding
@@ -41,7 +42,6 @@ def all_face_encoding():
         # load every user
         user_face_encoding = np.loadtxt('training-data/{0}/{1}_encoding.txt'.format(user, user))
         known_face_encodings.append(user_face_encoding)
-    print(known_face_encodings)
 
 
 # Initialize some variables
@@ -51,32 +51,40 @@ face_names = []
 face_log = {}
 process_this_frame = True
 reset = time.time() + 60 * 60 * 24
+
 all_face_encoding()
 
 while True:
-    # Grab a single frame of video
-    camera.capture(rawCapture, format='rgb')
-    frame = rawCapture.array
+    # grab an image from the camera
+    frame = cam.get_image()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Only process every other frame of video to save time
     if process_this_frame:
-        print("process new frame")
-
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(frame)
         face_encodings = face_recognition.face_encodings(frame, face_locations)
+
         face_names = []
         for face_encoding in face_encodings:
-            print(str(len(face_encodings)) + " face(s)")
-
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.5)
+            name = "Ptdr t ki"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
+            # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
-            name = "Ptdr t ki"
 
             if matches[best_match_index]:
                 name = user_faces_name[best_match_index]
+
             face_names.append(name)
 
     process_this_frame = not process_this_frame
@@ -88,6 +96,7 @@ while True:
         right *= 4
         bottom *= 4
         left *= 4
+
         today = datetime.datetime.today()
         if not name == 'Ptdr t ki' and not os.path.isfile("training-data/{0}/{1}.jpg".format(name, name)):
             location_for_update = 'training-data/{0}/{1}_encoding.txt'.format(name, name)
@@ -96,21 +105,31 @@ while True:
             if duration.seconds > 30:
                 # mettre a jour photo si date > 1 mois
                 cv2.imwrite('training-data/{0}/{1}.jpg'.format(name, name), frame)
+            os.system("python3 lock_control.py authorized " + name)
+        else:
+            os.system("python3 lock_control.py unauthorized")
         print(name)
+
         datestamp = today.strftime("%m/%d/%Y, %H:%M:%S")
         date = today.strftime("%m-%d-%Y")
         if not os.path.exists('log'):
             os.makedirs('log')
+
         if name not in face_log or time.time() > face_log[name]:
             face_log[name] = time.time() + 10
             mode = 'a' if os.path.isfile("log/" + date) else 'w'
             with open("log/" + date, mode) as log:
                 log.write(name + " / face / " + datestamp + "\n")
                 log.close()
+    # Display the resulting image
+    cv2.imshow("Image", frame)
 
+    cv2.imshow('Video', frame)
     if time.time() > reset:
         all_face_encoding()
         reset = time.time() + 60 * 60 * 24
 
-    rawCapture.truncate(0)
-    del frame
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
