ef3cf05e7dd4b575eb71efd8a9cdc8f2e42da8e7
Yamna Melki <melkiyamna@protonmail.com>
2020-03-03 11:54:13 +0100

    requests threading

--- api.py
+++ api.py
@@ -1,13 +1,29 @@
+import concurrent.futures
 import requests
+import threading
+import os
 import json
+import time
+
+thread_local = threading.local()
 
 
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
+def get_session():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
 
 
-response = requests.get("http://api.open-notify.org/astros.json")
-json_res = response.json()
+def authenticate():
+    url_login = os.getenv('API_LOGIN_ROUTE')
+    params = {"email": "yam@yam.fr", "password": "yam"}
+    session = get_session()
+    with session.post(url_login, params) as response:
+        token = json.loads(response.text)['token']
+    response = requests.post(url_login, params)
+    token = json.loads(response.text)['token']
 
-jprint(json_res)
+    headers = {
+        "Authorization": "Bearer " + token,
+        "content-type": "application/json",
+    }
