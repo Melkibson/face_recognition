9203ee8302d3139f5cba4abcd7a83d72c1a7d505
Yamna Melki <melkiyamna@protonmail.com>
2020-03-03 09:44:47 +0100

    create .env file for api route

--- qr_code.py
+++ qr_code.py
@@ -2,21 +2,18 @@ import cv2
 import pyzbar.pyzbar as pyzbar
 import requests
 import json
+import os
+from classifier import frame
 
 
-def qr_code_reader(code):
-    camera = cv2.VideoCapture(0)
-    capture = True
-    while capture:
-        ret, frame = camera.read()
-        decodedObjects = pyzbar.decode(frame)
-        while decodedObjects:
-            decoded = decodedObjects[0].data
-            if decoded == bytes(code, 'utf-8'):
-                print("Access granted")
-                break
-            else:
-                print("Access denied")
+def qr_code_reader(code, frame):
+    decodedObjects = pyzbar.decode(frame)
+    while decodedObjects:
+        decoded = decodedObjects[0].data
+        if decoded == bytes(code, 'utf-8'):
+            print("Access granted")
+        else:
+            print("Access denied")
 
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -25,8 +22,8 @@ def qr_code_reader(code):
 
 video_capture = cv2.VideoCapture(0)
 
-url_login = 'http://kanarpp.xyz:3000/user/login'
-url_qrcode = 'http://kanarpp.xyz:3000/qrcode'
+url_login = os.getenv('API_LOGIN_ROUTE')
+url_qrcode = os.getenv('API_QRCODE_ROUTE')
 
 params = {"email": "yam@yam.fr", "password": "yam"}
 
@@ -38,12 +35,13 @@ headers = {
     "Authorization": "Bearer " + token,
     "content-type": "application/json",
 }
+
 try:
     response = requests.get(url_qrcode, headers=headers)
     user_codes = json.loads(response.text)
     for user_code in user_codes['data']:
         qr_code = user_code['qrcode']
-        qr_code_reader(qr_code)
+        qr_code_reader(qr_code, frame)
 except requests.exceptions.ConnectionError:
     response.status_code = "Connection refused"
 
