4684103351a36ead358f3e7eb59be30cab6133a5
novaedra <leofouquier20111999@gmail.com>
2020-03-05 12:11:38 +0100

    solarization

--- solarization.py
+++ solarization.py
@@ -13,7 +13,6 @@ while not i == 5:
     i1 = Image.open("dorian1.jpg")
     i2 = solarize
     assert i1.mode == i2.mode, "Different kinds of images."
-    assert i1.size == i2.size, "Different sizes."
 
     pairs = zip(i1.getdata(), i2.getdata())
     if len(i1.getbands()) == 1:
@@ -21,6 +20,4 @@ while not i == 5:
         dif = sum(abs(p1 - p2) for p1, p2 in pairs)
     else:
         dif = sum(abs(c1 - c2) for p1, p2 in pairs for c1, c2 in zip(p1, p2))
-
-    ncomponents = i1.size[0] * i1.size[1] * 3
-    print("Difference (percentage):", (dif / 255.0 * 100) / ncomponents)
+    print("Difference (percentage):", (dif / 255.0 * 100))
