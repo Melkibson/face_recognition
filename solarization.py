b43a149cf0531e025a21103bd5abc5aaf427b74e
novaedra <leofouquier20111999@gmail.com>
2020-03-05 11:26:55 +0100

    solarization

--- solarization.py
+++ solarization.py
@@ -1,10 +1,30 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import cv2
+import numpy as np
 
-img = mpimg.imread('training-data/dorian/dorian.jpg')
-plt.imshow(img, cmap='gray')
-plt.savefig('gray.png')
+if __name__ == '__main__':
 
-lum_img = img[:, :, 0]
-plt.imshow(lum_img, cmap="hot")
-plt.savefig('lum_img.png')
+    # ルックアップテーブルを作成するため変数の定義
+    solarization_const = 2 * np.pi / 255
+
+    # ルックアップテーブルの生成
+    """
+    array([[0],
+           [0],
+           [0],
+           [0],
+           [0],
+           [0],
+           [0],
+        ・・・・・・・・
+           [0], dtype='uint8')
+    のようなリストが生成される。
+    """
+    look_up_table = np.ones((256, 1), dtype = 'uint8' ) * 0
+
+    for i in range(256):
+
+        look_up_table[i][0] = np.abs(np.sin(i * solarization_const)) * 100
+
+    img_src = cv2.imread("./image/sora2.jpg", 1)
+    img_sola = cv2.LUT(img_src, look_up_table)
+    cv2.imwrite("solarized.jpg", img_sola)
