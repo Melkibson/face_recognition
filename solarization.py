2e31ccadd90e6ab0f05ffb52d9565731644ecbff
novaedra <leofouquier20111999@gmail.com>
2020-03-05 11:35:44 +0100

    solarization

--- solarization.py
+++ solarization.py
@@ -2,9 +2,7 @@ from PIL import Image, ImageOps
 
 img = Image.open("training-data/dorian/dorian.jpg")
 gray = ImageOps.grayscale(img)
-equalize = ImageOps.equalize(gray, mask=None)
-solarize = ImageOps.solarize(equalize, threshold=128)
+solarize = ImageOps.solarize(gray, threshold=128)
 
 gray.save("gray.jpg")
-equalize.save("equalize.jpg")
 solarize.save("solarize.jpg")
