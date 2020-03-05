1767690cbfd1126b1d947201c666c3c3253308d2
novaedra <leofouquier20111999@gmail.com>
2020-03-05 11:16:51 +0100

    solarization

--- solarization.py
+++ solarization.py
@@ -2,8 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 img = mpimg.imread('training-data/dorian/dorian.jpg')
-img.savefig('img.png')
+img.savefig('gray.png')
 
 lum_img = img[:, :, 0]
-plt.imshow(lum_img, cmap="hot")
-plt.savefig('lum_img.png')
+lum_img.savefig('lum_img.png')
