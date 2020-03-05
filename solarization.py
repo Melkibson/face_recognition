14bee99fb288cd4fd9f94ed3b4b07e175dbc9fec
novaedra <leofouquier20111999@gmail.com>
2020-03-05 11:34:21 +0100

    solarization

--- solarization.py
+++ solarization.py
@@ -1,30 +1,10 @@
-import cv2
-import numpy as np
+from PIL import Image, ImageOps
 
-if __name__ == '__main__':
+img = Image.open("training-data/dorian/dorian.jpg")
+gray = ImageOps.grayscale(img)
+equalize = ImageOps.equalize(gray, mask=None)
+solarize = ImageOps.solarize(equalize, threshold=128)
 
-    # ルックアップテーブルを作成するため変数の定義
-    solarization_const = 2 * np.pi / 255
-
-    # ルックアップテーブルの生成
-    """
-    array([[0],
-           [0],
-           [0],
-           [0],
-           [0],
-           [0],
-           [0],
-        ・・・・・・・・
-           [0], dtype='uint8')
-    のようなリストが生成される。
-    """
-    look_up_table = np.ones((256, 1), dtype = 'uint8' ) * 0
-
-    for i in range(256):
-
-        look_up_table[i][0] = np.abs(np.sin(i * solarization_const)) * 100
-
-    img_src = cv2.imread("./image/sora2.jpg", 1)
-    img_sola = cv2.LUT(img_src, look_up_table)
-    cv2.imwrite("solarized.jpg", img_sola)
+gray.save("gray.jpg")
+equalize.save("equalize.jpg")
+solarize.save("solarize.jpg")
